a3eb2aaf0c632d5f42c10830a4dc2922cbb4a00a
Isabell Danell <imdanell@gmail.com>
2021-01-26 10:49:42 -0600

    deploying

---
+++ my-app/src/App.js
@@ -0,0 +1,109 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+
+
+
+        <p>
+          Well hey there! I'm Isabell Danell
+        </p>
+
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/isabell-danell"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View My Linkedin
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/imdanell"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View My Github
+        </a>
+
+
+
+      </header>
+
+      <body className="App-body">
+
+
+        <p>
+          Feel free to browse some of my previous projects!
+        </p>
+
+        <a
+          className="App-projects"
+          href="https://warm-wave-54519.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to view a note taker app built using Javascript and deployed through Heroku...
+        </a> <br></br>
+        <a
+          className="App-projects"
+          href="https://imdanell.github.io/UserDirectory-19/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to view a user directory built for employees to have easy access to their co-workers contact information...
+        </a> <br></br>
+        <a
+          className="App-projects"
+          href="https://imdanell.github.io/Password-Week3/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Test out a Javascript password generator I built by clicking this link!
+        </a> <br></br>
+        <a
+          className="App-projects"
+          href=" https://imdanell.github.io/DayPlanner-5/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to use my Javascript dayplanner which changes colors depending on the time of day.
+        </a> <br></br>
+        <a
+          className="App-projects"
+          href=" https://vast-gorge-32848.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow this link to view THE dad joke generator, a handlebars based group project I helped build the page layouts for, as well as the signup/login functions.
+        </a> <br></br>
+        <a
+          className="App-projects"
+          href=" https://imdanell.github.io/NewHTMLportfolio-16/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          OR, click on this link to see my new HTML/CSS skills at work
+        </a>
+
+      </body>
+
+      <footer className="App-footer">
+
+        <a>
+          Made with 🤍 by Isabell Danell
+        </a>
+
+
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
+
